db7444737630f3d816eca68736bed13f5cd040a6
Eric Tran <ericpro258@gmail.com>
2021-07-18 00:41:30 +1000

    Update wsgi.py

--- P117/wsgi.py
+++ P117/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P117.settings')
-
+sys.path.append('/opt/bitnami/projects/P117')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/P117/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "P117.settings")
 application = get_wsgi_application()
